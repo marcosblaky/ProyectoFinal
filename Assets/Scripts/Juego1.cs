74d3d025c1f7c89524e5db866390b0900484a48e
Marcos <marcos_castano@ceroca.cat>
2025-03-10 20:01:04 +0100

    a

--- Assets/Scripts/Juego1.cs
+++ Assets/Scripts/Juego1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,11 @@ public class Juego1 : MonoBehaviour
 {
 
     public Button[] buttons;
+    // hacer tabla 5x5 de booleanos que son todo 0
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +23,11 @@ public class Juego1 : MonoBehaviour
     void Update()
     {
         
+        //que los botones comprueben si el que has apretado es el del color distinto (1 en la tabla) y si es así actualicen colores.
+
     }
+
+    //hacer funcion que cambie el booleano de la tabla que si es un 1 a uno aleatorio
 
     public void ChangeColorButtons(Color newColor)
     {

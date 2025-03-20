b22aadf7f4ffb291f1b4b4926063f9315b1e2d3c
Marcos <marcos_castano@ceroca.cat>
2025-03-20 05:03:01 +0100

    comentarios de que hacer más tarde

---
+++ Assets/Scripts/juego2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class juego2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // es crear 2 contadores, uno para la cuenta atrás y otro para la puntuacion. puner un button con texto "no pulses aún" que cuando el primer
+        //contador llega a 0 se cambia por "PULSA", el juego termina si pulsa el boton, si lo pulsa antes de tiempo la puntuacion es 0, si lo pulsa 
+        //despues la puntuacion es la diferencia de tiempo de los contadores (el de cuenta atrás se pausa al acabar la cuenta atrás).
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

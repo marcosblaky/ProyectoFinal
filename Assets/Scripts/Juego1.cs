143167ef6c65e6f6e0fdc78846918772beb02635
Marcos <marcos_castano@ceroca.cat>
2025-03-27 16:39:18 +0100

    Juego1 botones y colores conectados

--- Assets/Scripts/Juego1.cs
+++ Assets/Scripts/Juego1.cs
@@ -1,40 +1,69 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Juego1 : MonoBehaviour
 {
+    public Button[] buttons; // Debe tener 25 botones en el Inspector
+    private bool[,] tabla = new bool[5, 5];
 
-    public Button[] buttons;
-    // hacer tabla 5x5 de booleanos que son todo 0
-
-
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        ChangeColorButtons(Color.red);
-    }
+        CambiarAleatorio();
+        ActualizarColores();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-        //que los botones comprueben si el que has apretado es el del color distinto (1 en la tabla) y si es así actualicen colores.
-
-    }
-
-    //hacer funcion que cambie el booleano de la tabla que si es un 1 a uno aleatorio
-
-    public void ChangeColorButtons(Color newColor)
-    {
-        foreach (Button btn in buttons)
+        // Asignar eventos a los botones
+        for (int i = 0; i < buttons.Length; i++)
         {
-            btn.image.color = newColor;
+            int index = i; // Captura el índice para la lambda
+            buttons[i].onClick.AddListener(() => VerificarBoton(index));
         }
     }
 
+    void CambiarAleatorio()
+    {
+        System.Random rnd = new System.Random();
+
+        // Poner todos los valores en false
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                tabla[i, j] = false;
+            }
+        }
+
+        // Seleccionar una posición aleatoria y ponerla en true
+        int fila = rnd.Next(0, 5);
+        int columna = rnd.Next(0, 5);
+        tabla[fila, columna] = true;
+    }
+
+    void ActualizarColores()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                int index = i * 5 + j; // Convertir coordenadas 2D a 1D para acceder al array de botones
+                if (buttons[index] != null)
+                {
+                    buttons[index].image.color = tabla[i, j] ? Color.green : Color.red;
+                }
+            }
+        }
+    }
+
+    void VerificarBoton(int index)
+    {
+        int fila = index / 5;
+        int columna = index % 5;
+
+        // Si el botón corresponde al 1 en la tabla, actualizar los colores con una nueva posición aleatoria
+        if (tabla[fila, columna])
+        {
+            CambiarAleatorio();
+            ActualizarColores();
+        }
+    }
 }
